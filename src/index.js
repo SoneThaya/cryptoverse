4a021a9a6479512130b25b7def88a9ca65408c1b
sone <sthaya@gmail.com>
2021-11-14 18:59:14 -0800

    setup redux tool kit to fetch data

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDom from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import App from "./App";
+import store from "./app/store";
 import "antd/dist/antd.css";
 
 ReactDom.render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/*" element={<App />} />
-    </Routes>
+    <Provider store={store}>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </Provider>
   </BrowserRouter>,
   document.getElementById("root")
 );
